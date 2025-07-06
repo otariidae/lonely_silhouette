4e1920698e73c4a94daa43399c666841b4cd1f71
otariidae <otariidae@users.noreply.github.com>
2025-07-06 19:24:04 +0900

    style: ruff format

--- tests/test_part_of_speech.py
+++ tests/test_part_of_speech.py
@@ -8,9 +8,15 @@ from lonely_silhouette.part_of_speech import PartOfSpeech
         ("名詞,一般,*,*", "名詞", "一般", "*", "*"),
         ("名詞,固有名詞,地域,一般", "名詞", "固有名詞", "地域", "一般"),
         ("動詞,自立,*,*", "動詞", "自立", "*", "*"),
-    ]
+    ],
 )
-def test_from_raw_part_of_speech(raw_part_of_speech, expected_category, expected_subcategory1, expected_subcategory2, expected_subcategory3):
+def test_from_raw_part_of_speech(
+    raw_part_of_speech,
+    expected_category,
+    expected_subcategory1,
+    expected_subcategory2,
+    expected_subcategory3,
+):
     part_of_speech = PartOfSpeech.from_raw_part_of_speech(raw_part_of_speech)
     assert part_of_speech.category == expected_category
     assert part_of_speech.subcategory1 == expected_subcategory1
